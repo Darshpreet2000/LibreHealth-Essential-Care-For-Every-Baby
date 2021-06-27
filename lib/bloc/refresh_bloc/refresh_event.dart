2a319fa1b9e739770df0d2ee742aefe3b6a5ca58
Darshpreet Singh <dsingh200021@gmail.com>
2021-06-27 16:45:53 +0530

    Create refresh_bloc, repository

---
+++ lib/bloc/refresh_bloc/refresh_event.dart
@@ -0,0 +1,8 @@
+part of 'refresh_bloc.dart';
+
+abstract class RefreshEvent {
+  const RefreshEvent();
+}
+class RefreshEventStart extends RefreshEvent{
+  
+}
---
+++ lib/repository/refresh_repository.dart
@@ -0,0 +1,43 @@
+import 'package:newborn_care/main.dart';
+import 'package:newborn_care/models/network_request.dart';
+import 'package:newborn_care/network/refresh_client.dart';
+import 'package:newborn_care/repository/hive_storage_repository.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:http/http.dart' as http;
+class RefreshRepository {
+  Map<String, String> m = new Map();
+  RefreshRepository() {
+    m.addAll({
+      "noInternetConnection":
+          AppLocalizations.of(scaffoldMessengerGlobalKey.currentContext!)!
+              .noInternetConnection,
+      "invalidRequest":
+          AppLocalizations.of(scaffoldMessengerGlobalKey.currentContext!)!
+              .invalidRequest,
+      "unauthorised":
+          AppLocalizations.of(scaffoldMessengerGlobalKey.currentContext!)!
+              .unauthorised,
+      "invalidInput":
+          AppLocalizations.of(scaffoldMessengerGlobalKey.currentContext!)!
+              .invalidInput,
+      "errorDuringCommunication":
+          AppLocalizations.of(scaffoldMessengerGlobalKey.currentContext!)!
+              .errorDuringCommunication
+    });
+  }
+  Future<void> startRefreshing() async {
+    try {
+      List<NetworkRequest> networkRequests =
+          HiveStorageRepository().getNetworkRequests();
+      while (networkRequests.isNotEmpty) {
+        NetworkRequest request = networkRequests.first;
+
+        await RefreshClient(http.Client(), m).doNetworkRequest(request);
+        networkRequests.removeAt(0);
+        HiveStorageRepository().storeNetworkRequestList(networkRequests);
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+}
