2524d5eb2b6fe58d8c7328eb46c31521abd6ba6f
Darshpreet Singh <dsingh200021@gmail.com>
2021-07-01 22:03:21 +0530

    Created phase 3 & 4 assessments

---
+++ lib/screens/baby_assessments/components/assessments_phases/phase_3_normal.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Phase3Normal extends StatelessWidget {
+  const Phase3Normal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      width: double.infinity,
+      decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+          color: Theme.of(context).scaffoldBackgroundColor,
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              "The baby has been classified as Normal",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Following interventions to be taken:",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.green[100],
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: CheckboxListTile(
+                  title: Text(
+                    "Maintain Normal Temperature",
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                  ),
+                  value: false,
+                  onChanged: (newValue) {},
+                  controlAffinity: ListTileControlAffinity.trailing,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.green[100],
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: CheckboxListTile(
+                  title: Text(
+                    "Support BreastFeeding",
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                  ),
+                  value: false,
+                  onChanged: (newValue) {},
+                  controlAffinity: ListTileControlAffinity.trailing,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.green[100],
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: CheckboxListTile(
+                  title: Text(
+                    "Advise About BreastFeeding Problems",
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                  ),
+                  value: false,
+                  onChanged: (newValue) {},
+                  controlAffinity: ListTileControlAffinity.trailing,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.green[100],
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: CheckboxListTile(
+                  title: Text(
+                    "Immunize",
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                  ),
+                  value: false,
+                  onChanged: (newValue) {},
+                  controlAffinity: ListTileControlAffinity.trailing,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/baby_assessments/components/toggle_buttons/swtich_yes_no.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class SwtichYesNo extends StatefulWidget {
+  final String first, second;
+  final List<bool> isSelected;
+
+  SwtichYesNo(this.first, this.second, this.isSelected);
+
+  @override
+  _SwtichYesNoState createState() => _SwtichYesNoState();
+}
+
+class _SwtichYesNoState extends State<SwtichYesNo> {
+  @override
+  Widget build(BuildContext context) {
+    List<bool> isSelected = widget.isSelected;
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      side: BorderSide(color: Colors.blue, width: 2.5),
+                      primary: isSelected[0] ? Colors.blue : null,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(30.0))),
+                  onPressed: () {
+                    setState(() {
+                      isSelected[1] = false;
+                      isSelected[0] = true;
+                    });
+                  },
+                  child: Text(
+                    widget.first,
+                    style: TextStyle(
+                      color: isSelected[0]
+                          ? Colors.white
+                          : Theme.of(context).textTheme.subtitle1!.color,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      side: BorderSide(color: Colors.blue, width: 2.5),
+                      primary: isSelected[1] ? Colors.blue : null,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(30.0))),
+                  onPressed: () {
+                    setState(() {
+                      isSelected[0] = false;
+                      isSelected[1] = true;
+                    });
+                  },
+                  child: Text(
+                    widget.second,
+                    style: TextStyle(
+                      color: isSelected[1]
+                          ? Colors.white
+                          : Theme.of(context).textTheme.subtitle1!.color,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
