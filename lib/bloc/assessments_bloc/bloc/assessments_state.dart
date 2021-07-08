74f301b56684d4f2f1e13e02eb40e475ad7eba36
Darshpreet Singh <dsingh200021@gmail.com>
2021-07-08 21:47:23 +0530

    unit test for phase - 1 assessments, bloc, client, repository

--- lib/bloc/assessments_bloc/bloc/assessments_state.dart
+++ lib/bloc/assessments_bloc/bloc/assessments_state.dart
@@ -1,6 +1,6 @@
 part of 'assessments_bloc.dart';
 
-abstract class AssessmentsState {
+abstract class AssessmentsState extends Equatable{
   const AssessmentsState();
 
   get childModel => null;
@@ -10,22 +10,34 @@ class AssessmentsInitial extends AssessmentsState {
   final ChildModel childModel;
 
   AssessmentsInitial(this.childModel);
+
+  @override
+  List<Object?> get props => [];
 }
 
 class AssessmentsAdded extends AssessmentsState {
   final ChildModel childModel;
 
   AssessmentsAdded(this.childModel);
+
+  @override
+  List<Object?> get props => [];
 }
 
 class AssessmentsError extends AssessmentsState {
   final String message;
 
   AssessmentsError(this.message);
+
+  @override
+  List<Object?> get props => [];
 }
 
 class AssessmentsLoading extends AssessmentsState {
   final ChildModel childModel;
 
   AssessmentsLoading(this.childModel);
+
+  @override
+  List<Object?> get props => [];
 }
